4522574714c24bbb044efe6a7a6e723f42c85493
jasonhch <jason71319jason@gmail.com>
2025-07-12 17:04:57 +0800

    update neovim setting

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,2 +1,17 @@
-# lazy.nvim
+-- setting
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+-- lazy.nvim
 require("config.lazy")
